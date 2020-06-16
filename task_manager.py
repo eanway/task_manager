2f46387331bf74dbc08a0b7eac3735d6233f075f
Evan Cutler Anway <eca7372@uncw.edu>
2020-06-15 21:57:22 -0400

    added function to sum task hours. changed chunk inputs to floats instead of integers.

--- task_manager.py
+++ task_manager.py
@@ -27,13 +27,18 @@ class Task(object):
             )
         )
 
-    def chunk_hours(self, minimum_hours=1):
-        if not isinstance(self.hours, list):
-            number_of_chunks = math.floor(self.hours / minimum_hours)
-            remainder = self.hours % minimum_hours
-            chunks = [minimum_hours] * number_of_chunks
-            chunks[-1] += remainder
-            self.hours = chunks
+    def chunk_hours(self, minimum_hours=1.0):
+        if isinstance(self.hours, list):
+            self.sum_hours()
+
+        number_of_chunks = math.floor(self.hours / minimum_hours)
+        remainder = self.hours % minimum_hours
+        chunks = [minimum_hours] * number_of_chunks
+        chunks[-1] += remainder
+        self.hours = chunks
+
+    def sum_hours(self):
+        self.hours = sum(self.hours)
 
 
 class Schedule(object):
@@ -51,7 +56,7 @@ class Schedule(object):
         for task in self.tasks:
             task.print_details()
 
-    def chunk_tasks(self, minimum_time=1):
+    def chunk_tasks(self, minimum_time=1.0):
         """Chunk the tasks into shorter durations."""
         for task in self.tasks:
             task.chunk_hours(minimum_time)
@@ -61,5 +66,6 @@ my_task = Task("code", "6/15/2020", 8)
 my_task.chunk_hours(0.75)
 my_second_task = Task("eat", "6/16/2020", 16)
 my_schedule = Schedule([my_task, my_second_task])
+my_schedule.print_details()
 my_schedule.chunk_tasks()
 my_schedule.print_details()
